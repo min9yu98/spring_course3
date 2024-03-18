27dd28ecd5c289ceeb690169902909d2a3e8f6fa
Min Gyu <kimmingyu9811@gmail.com>
2024-03-18 13:26:15 +0900

    feat: first commit & 서블릿

---
+++ src/main/java/hello/servlet/basic/HelloData.java
@@ -0,0 +1,13 @@
+package hello.servlet.basic;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class HelloData {
+    private String username;
+    private int age;
+}
