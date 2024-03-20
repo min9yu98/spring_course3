7f460a3a45f46aea0beaa057fefbcc766beafefb
Min Gyu <kimmingyu9811@gmail.com>
2024-03-20 14:42:11 +0900

    fix: viewPath에서 누락된 / 추가

--- src/main/java/hello/servlet/web/frontcontroller/MyView.java
+++ src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -15,18 +15,18 @@ public class MyView {
         this.viewPath = viewPath;
     }
 
-    private static void modelToRequestAttribute(Map<String, Object> model, HttpServletRequest request) {
-        model.forEach((key, value) -> request.setAttribute(key, value));
-    }
-
     public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
         dispatcher.forward(request, response);
     }
-    
+
     public void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         modelToRequestAttribute(model, request);
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
         dispatcher.forward(request, response);
+    }
+
+    private void modelToRequestAttribute(Map<String, Object> model, HttpServletRequest request) {
+        model.forEach((key, value) -> request.setAttribute(key, value));
     }
 }
